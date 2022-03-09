fd72a085d216286cd84a392185751bb9f29faccb
Trey Herrera <therrera152@gmail.com>
2022-03-09 08:54:20 -0600

    commit tree 09354663c47313bb9d3de46873d0d36e31221cb1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+//removes element main#main
+
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.innerHTML="Trey is the champion!";
+newHeader.className="victory";
